be09996ec19fc6ab3e80addbe5c87d588b82b8cd
Supertang8 <38047420+Supertang8@users.noreply.github.com>
2021-10-12 09:17:49 +0200

    Update test_servo.ino

--- test_servo/test_servo.ino
+++ test_servo/test_servo.ino
@@ -1,57 +1,39 @@
-//#include <Servo.h>
+#include <Servo.h>
 #define SERVO_PIN 11
 
-//Servo servo;
-
-//int servo = 11;
-int echo = 6;
+Servo servo;
 int Trig = 6;
 int a = 0;
 bool forward = true;
 
 void setup() {
-//  pinMode(servo, OUTPUT);  
-  //pinMode(Trig, OUTPUT);
-  //pinMode(echo, INPUT);
   Serial.begin(9600);
-  //servo.attach(SERVO_PIN);
+  servo.attach(SERVO_PIN);
   
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-/*  PROGRAM TO TURN FROM SIDE TO SIDE:
-  servo.write(80);
-  delay(1000);
-  servo.write(179);
-  delay(1000);
-  servo.write(80);
-  delay(1000);
-  servo.write(2);
-  delay(1000);
-  *//*
   //Program to turn towards thing.
   int closestDistance = 300;
   int closestAngle = 80;
-  Serial.println(closestDistance);
   for(int angle = 2; angle < 180; angle++)
   {
     servo.write(angle);
     int currentDistance = Distance_test();
-    if(currentDistance < closestDistance)
+    if(currentDistance <= closestDistance)
     {
       closestAngle = angle;
       closestDistance = currentDistance;
     }
     delay(10);
-    Serial.println(closestDistance);
-    servo.write(currentDistance);
+    Serial.print("Closest distance = ");
+    Serial.print(closestDistance);
+    Serial.print(" at angle = ");
+    Serial.println(closestAngle);
+    servo.write(closestAngle);
   }
   
   delay(5000);
-  */
-  Serial.println(Distance_test());
-  delay(100);
 }
 
 // PROGRAM COPIED FROM https://maker.pro/arduino/tutorial/an-ultrasonic-object-following-robot
@@ -61,9 +43,9 @@ int Distance_test() {
   delayMicroseconds(2);
   digitalWrite(Trig, HIGH);  
   delayMicroseconds(20);
-  digitalWrite(Trig, LOW);
-  pinMode(echo, INPUT);
-  float Fdistance = pulseIn(echo, HIGH);  
-  Fdistance= Fdistance / 58.0;       
+  digitalWrite(Trig, LOW);   
+  pinMode(Trig, INPUT);
+  float Fdistance = pulseIn(Trig, HIGH);  
+  Fdistance= Fdistance / 58;       
   return (int)Fdistance;
 }
